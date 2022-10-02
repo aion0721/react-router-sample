4f8eac9e65b69da6d84e1f0cf86f9a11240a0c7a
a <rudolf.aion@gmail.com>
2022-10-02 10:59:54 +0000

    parameter

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,5 +1,6 @@
+import { Link  } from "react-router-dom"
 export const Page2 = () =>{
     return(
-        <div><h1>Page2Page</h1></div>
+        <div><h1>Page2Page</h1><Link to="/page2/100">URL Parameter</Link></div>
     )
 }
---
+++ src/UrlParameter.jsx
@@ -0,0 +1,8 @@
+import { useParams } from 'react-router-dom'
+
+export const UrlParameter = () => {
+    const { id } = useParams();
+    return(
+        <div><h1>UrlParameter</h1><p>Parameter is {id}.</p></div>
+    )
+}
