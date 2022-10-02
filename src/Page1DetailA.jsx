3e0076e72784ffe137c23cd3b09f891909abd48c
a <rudolf.aion@gmail.com>
2022-10-02 11:20:56 +0000

    go back

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const location = useLocation();
+  const history = useHistory();
   console.log(location);
+  const onClickBack = () => {
+    history.goBack();
+  };
   return (
     <div>
       <h1>Page1DetailAPage</h1>
+      <button onClick={onClickBack}>GoBack</button>
     </div>
   );
 };
