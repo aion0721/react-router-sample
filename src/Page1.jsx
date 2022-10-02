e841e9b71d612cd9c52d8f0b22e1e0bb79f819c8
a <rudolf.aion@gmail.com>
2022-10-02 10:52:19 +0000

    separate routes

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -2,6 +2,6 @@ import { Link } from "react-router-dom"
 
 export const Page1 = () =>{
     return(
-        <div><h1>Page1Page</h1><Link to="/page1/detaila">DetailA</Link><Link to="/page1/detailb">DetailB</Link></div>
+        <div><h1>Page1Page</h1><Link to="/page1/page1detailA">DetailA</Link><Link to="/page1/page1detailB">DetailB</Link></div>
     )
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Switch, Route } from "react-router-dom"
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from '../Page1DetailA';
-import { Page1DetailB } from '../Page1DetailB';
 import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
 export const Router = () => {
     return(
       <Switch>
@@ -12,9 +10,9 @@ export const Router = () => {
         </Route>
         <Route path='/page1' render={({match: {url}}) => (
           <Switch>
-            <Route exact path={url}><Page1 /></Route>
-            <Route exact path={`${url}/detailA`}><Page1DetailA /></Route>
-            <Route exact path={`${url}/detailB`}><Page1DetailB /></Route>
+            {page1Routes.map( (route) => (
+                <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>{route.children}</Route>
+                ))}
           </Switch>
          )}
         />
