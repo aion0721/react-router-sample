580ac7ed6ff7cf2d0d249d6a49ccb013e3d3a915
a <rudolf.aion@gmail.com>
2022-10-02 11:16:40 +0000

    props state

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export const Page1 = () =>{
-    return(
-        <div><h1>Page1Page</h1><Link to="/page1/page1detailA">DetailA</Link><Link to="/page1/page1detailB">DetailB</Link></div>
-    )
-}
+export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  return (
+    <div>
+      <h1>Page1Page</h1>
+      <Link to={{ pathname: "/page1/page1detailA", state: arr }}>DetailA</Link>
+      <Link to="/page1/page1detailB">DetailB</Link>
+    </div>
+  );
+};
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,6 +1,11 @@
+import { useLocation } from "react-router-dom";
 
-export const Page1DetailA = () =>{
-    return(
-        <div><h1>Page1DetailAPage</h1></div>
-    )
-}
+export const Page1DetailA = () => {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div>
+      <h1>Page1DetailAPage</h1>
+    </div>
+  );
+};
